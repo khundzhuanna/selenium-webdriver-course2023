77cbee09745ddb9d18c3c3a7be36ea1f5121c4c3
anna <default@mail.ru>
2023-04-23 14:47:08 +0300

    LoginScenario

---
+++ project/src/test/java/LoginScenario.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginScenario {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/ruaaaye/Tools/chromedriver.exe");
+        driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void LoginScenario() {
+        driver.navigate().to("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.tagName("button").name("login")).click();
+    }
+
+    @AfterMethod
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
