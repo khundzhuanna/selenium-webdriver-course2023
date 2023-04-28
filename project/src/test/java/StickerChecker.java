d0e7b126377028164d51fc104f1f7665386bffb4
anna <default@mail.ru>
2023-04-28 18:39:05 +0300

    added StickerChecker

---
+++ project/src/test/java/StickerChecker.java
@@ -0,0 +1,57 @@
+import java.util.List;
+import java.lang.RuntimeException;
+import java.util.NoSuchElementException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class StickerChecker {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/ruaaaye/Tools/chromedriver.exe");
+        driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void stickerChecker() {
+        driver.navigate().to("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.navigate().to("http://localhost/litecart/en/");
+
+        List<WebElement> elements = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
+        for (int j = 0; j < elements.size(); j++) {
+            WebElement ducky = elements.get(j); // - достаём уточку
+            List<WebElement> stickerDuckyNew = ducky.findElements(By.xpath(".//div[@class='sticker new']"));
+            List<WebElement> stickerDuckySale = ducky.findElements(By.xpath(".//div[@class='sticker sale']"));
+
+            if (stickerDuckyNew.size() == 0 && stickerDuckySale.size() == 0) {
+                throw new NoSuchElementException();
+            } else if (stickerDuckyNew.size() > 1 && stickerDuckySale.size() == 0) {
+                throw new RuntimeException("More than one 'New' sticker!");
+            } else if (stickerDuckyNew.size() == 0 && stickerDuckySale.size() > 1) {
+                throw new RuntimeException("More than one 'Sale' sticker!");
+            } else if (stickerDuckyNew.size() >= 1 && stickerDuckySale.size() >= 1) {
+                throw new RuntimeException("Both 'Sale' and 'New' stickers added!");
+            }
+        }
+    }
+
+    @AfterMethod
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
+
